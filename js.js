f27d0b2e3f0217852cca26a2a5c9828e85927eee
Sandrouser <dzamashvilisandro@gmail.com>
2024-04-14 17:32:45 +0400

    kalkulatori5

--- js.js
+++ js.js
@@ -1,9 +1,16 @@
 let h1=document.querySelector("h1")
-console.dir(h1)
+let span1=document.querySelector(".span1")
+let span2=document.querySelector(".span2")
+let span3=document.querySelector(".span3")
+let body=document.querySelector("body")
+console.dir(body)
 function display(x){
     console.log(h1.textContent.length)
     if(h1.textContent.length<17){
-         h1.textContent=h1.textContent+x
+         if(h1.textContent==="" && x==="+"){
+            h1.textContent=""
+         }
+         else{ h1.textContent=h1.textContent+x}
      }
     // h1.textContent=h1.textContent+x
 }
@@ -16,3 +23,25 @@ function tolia(){
 function del(){
     h1.textContent=h1.textContent.slice(0,-1)
 }
+span1.addEventListener("click", () =>{
+    span1.style.opacity="1"
+    span2.style.opacity="0"
+    span3.style.opacity="0"
+    body.classList.add("tema1")
+    body.classList.remove("tema2")
+})
+
+span2.addEventListener("click", () =>{
+    span1.style.opacity="0"
+    span2.style.opacity="1"
+    span3.style.opacity="0"
+    body.classList.remove("tema2")
+    body.classList.remove("tema1")
+})
+span3.addEventListener("click", () =>{
+    span1.style.opacity="0"
+    span2.style.opacity="0"
+    span3.style.opacity="1"
+    body.classList.add("tema2")
+    body.classList.remove("tema1")
+})
